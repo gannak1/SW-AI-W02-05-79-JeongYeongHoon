4a3db72c590b52bf6246583f30de180397e127ef
gannak1 <gi676788@gmail.com>
2026-03-07 23:41:00 +0900

    브론즈3 문제

--- week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
+++ week2/problem-solving/난이도하_파이썬문법_최댓값_브론즈3.py
@@ -1,2 +1,15 @@
 # 파이썬 문법 - 최댓값 (백준 브론즈3)
 # 문제 링크: https://www.acmicpc.net/problem/2562
+
+problem_array = []
+for i in range(9):
+    problem_array.append(int(input()))
+
+
+max_number = -1
+max_index = -1
+for i in range(len(problem_array)):
+    if max_number < problem_array[i]:
+        max_number = problem_array[i]
+        max_index = i + 1
+print(f"{max_number}\n{max_index}\n")
