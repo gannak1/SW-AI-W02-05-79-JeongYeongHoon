b4b78daa2940b50d0a0c564b46307c0520c7d3cf
gannak1 <gi676788@gmail.com>
2026-03-10 20:55:20 +0900

    제곱ㄴㄴ 완료

--- week2/problem-solving/난이도상_백트래킹_비숍_플래5.py
+++ week2/problem-solving/난이도상_백트래킹_비숍_플래5.py
@@ -1,2 +1,49 @@
 # 백트래킹 - 비숍 (백준 플래5)
 # 문제 링크: https://www.acmicpc.net/problem/1799
+
+input_chess = [
+    [1, 1, 0, 1, 1],
+    [0, 1, 0, 0, 0],
+    [1, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0],
+    [1, 0, 1, 1, 1],
+]
+
+# input_chess = []
+# for i in range(int(input())):
+#     input_chess.append(list(map(int, input().split())))
+
+
+def main(input_chess):
+    result = []
+    col_location = [-1] * len(input_chess)
+    diag1 = [-1] * len(input_chess) * 2
+    diag2 = [-1] * len(input_chess) * 2
+    max_count = [-1]
+
+    def backtracking(row, count):
+
+        for i in range(len(input_chess)):
+            if max_count[0] < count:
+                max_count[0] = count
+            if (
+                (col_location[i] != 1)
+                # and (diag1[row + i] != 1)
+                # and (diag1[row - i + len(input_chess)] != 1)
+                and input_chess[row][i] == 1
+            ):
+                col_location[i] = 1
+                diag1[row - i + len(input_chess)] = 1
+                diag1[row + i] = 1
+                count += 1
+                backtracking(row + 1, count)
+                col_location[i] = -1
+                diag1[row - i + len(input_chess)] = -1
+                diag1[row + i] = -1
+                count -= 1
+
+    backtracking(0, 0)
+    return max_count
+
+
+print(main(input_chess)[0])
--- week2/problem-solving/난이도상_정수론_제곱ㄴㄴ수_골드1.py
+++ week2/problem-solving/난이도상_정수론_제곱ㄴㄴ수_골드1.py
@@ -1,2 +1,22 @@
 # 정수론 - 제곱 ㄴㄴ 수 (백준 골드1)
 # 문제 링크: https://www.acmicpc.net/problem/1016
+
+import math
+
+min_max_list = list(map(int, input().split()))
+
+count = 0
+
+total_array = [True] * (min_max_list[1] - min_max_list[0] + 1)
+
+for i in range(2, int(math.sqrt(min_max_list[1])) + 1):
+
+    start = math.ceil(min_max_list[0] / (i**2)) * (i**2) - min_max_list[0]
+
+    for j in range(len(total_array)):
+        if (len(total_array)) <= start + i**2 * j:
+            break
+        total_array[start + j * i**2] = False
+
+
+print(sum(total_array))
