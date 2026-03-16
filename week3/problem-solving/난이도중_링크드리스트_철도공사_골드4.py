30362ce82bc49540f73d47d2e8bc0bfdfe97b6a0
gannak1 <gi676788@gmail.com>
2026-03-16 11:46:00 +0900

    곱셈 완료

--- week3/problem-solving/난이도중_링크드리스트_철도공사_골드4.py
+++ week3/problem-solving/난이도중_링크드리스트_철도공사_골드4.py
@@ -1,2 +1,33 @@
 # 링크드리스트 - 철도 공사 (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/23309
+
+import sys
+
+n, m = list(map(int, sys.stdin.readline().rstrip("\n").split()))
+station_num_list = sys.stdin.readline().rstrip("\n").split()
+
+work_list = []
+for _ in range(m):
+    work_list.append(sys.stdin.readline().rstrip("\n").split())
+
+
+
+class Node:
+    def __init__(self, station_num):
+        self.station_num = station_num
+        self.next = None
+        self.prev = None
+
+
+class LinkedList:
+    def __init__(self,arr:list[str]):
+        a
+    
+    def bn_function(work):
+        i,j = work[1:]
+    def bp_function(work):
+        i,j = work[1:]
+    def cp_function(work):
+        i = work[1]
+    def cn_function(work):
+        i = work[1]
--- week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
+++ week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
@@ -1,2 +1,21 @@
 # 분할정복 - 곱셈 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/1629
+
+import sys
+
+data_list = list(map(int, sys.stdin.readline().rstrip("\n").split()))
+
+
+def divide_conquer(data, pow_data,div_data):
+    if pow_data == 1:
+        return (data) % div_data
+    elif pow_data % 2 == 0:
+        half = divide_conquer(data, pow_data // 2,div_data)
+        return (half * half) % div_data
+
+    elif pow_data % 2 != 0:
+            half = divide_conquer(data, (pow_data - 1) // 2,div_data)
+            return (half * half * data) % div_data
+
+
+print(divide_conquer(data_list[0], data_list[1], data_list[2]))
