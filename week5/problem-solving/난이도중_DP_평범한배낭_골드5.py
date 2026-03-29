d045d1367113fbdf509b457b336e9f63e95562d4
gannak1 <gi676788@gmail.com>
2026-03-29 22:27:18 +0900

    배낭완료

--- week5/problem-solving/난이도중_DP_평범한배낭_골드5.py
+++ week5/problem-solving/난이도중_DP_평범한배낭_골드5.py
@@ -1,23 +1,35 @@
 # DP - 평범한 배낭 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/12865
-# DP - 피보나치 수 2 (백준 브론즈 1)
-# 문제 링크: https://www.acmicpc.net/problem/2748
 
 import sys
 
-n = int(sys.stdin.readline().strip())
+input = sys.stdin.readline
+number_of_material, weight_limit = map(int, input().strip().split())
 
-def fibo(n,memo=None):
-    if memo == None:
-        memo = {}
-    if n == 0:
-        return 0
-    elif n <= 2:
-        return 1
-    if memo.get(n):
-        return memo.get(n)
-    else:
-        memo[n] = fibo(n-1,memo=memo) + fibo(n-2,memo=memo)
-        return memo[n]
-    
-print(fibo(n))
+material = [(0, 0)]
+
+for _ in range(number_of_material):
+    m = tuple(map(int, input().strip().split()))
+    material.append(m)
+
+dp = {}
+for w in range(weight_limit+1):
+    if not dp.get(0):
+        dp[0] = []
+    dp[0].append(0)
+max_value = 0
+for m in range(1,len(material)):
+    if not dp.get(m):
+        dp[m] = []
+        dp[m].append(0)
+    for w in range(1,weight_limit + 1):
+        weight = material[m][0]
+        value = material[m][1]
+        if w < weight:
+            dp[m].append(dp[m-1][w])
+        else:
+            dp[m].append(max(dp[m-1][w-weight] + value, dp[m-1][w]))
+
+max_value = dp[m][w]
+print(max_value)
+
